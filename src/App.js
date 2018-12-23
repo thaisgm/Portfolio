566f2b054c67df983fcffc8b4560c8af73add692
Thais Gonzalez <thaisgm@umich.edu>
2018-12-23 14:58:11 -0500

    Adding About Me

--- src/App.js
+++ src/App.js
@@ -59,7 +59,26 @@ class App extends Component {
                 </ListItem>
               </List>
             </div>
-            <p className = "title">Thais Gonzalez</p>
+            <p className = "body"> My name is Thais Gonzalez.
+            I grew up in South Florida, playing sports for most of my life and
+            graduated from St. Thomas Aquinas High School as a first-team all
+            state player with nearly a 5.0 GPA. I committed to play softball at
+            the University of Michigan, and was accepted into the Ross School of
+            Business. After my freshman year,I found that I was exactly passionate
+            about accounting, finance, or operations management. I decided to
+            explore a field that I had always love, but never dove into: technology.
+            After my first structured programming class, I was hooked.
+            But after that class, essentially all I was capable of doing was
+            coding board games in the console of XCode. I wanted to
+            gain actionable and applicable coding knowledge, and applied for a
+            Software Engineering Fellowship at Horizons School of Technology in
+            San Francisco. I was accepted and spent 12 weeks in SF studying
+            mobile and web technologies and working on many technical projects.
+            I found that I really enjoyed brainstorming with teams and creating a
+            solution with the customer's needs in mind, so when I returned to
+            Michigan, I decided to pursue my MicroMasters in User Experience,
+            Research, and Design as well as a minor in Computer Science. I created
+            this website to show the projects I've worked on. Thanks for stopping by, and Go Blue!</p>
         </div>
       );
     } else if (this.state.projectsPage){
