9954fb8da5450a88d3bfb622420da50fb7e12d83
Thais Gonzalez <thaisgm@umich.edu>
2018-12-22 23:03:17 -0500

    Adding Menu bar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import InfoIcon from '@material-ui/icons/Info';
 import './App.css';
 
 class App extends Component {
@@ -8,6 +14,28 @@ class App extends Component {
           <p>
             Thais Gonzalez
           </p>
+          <div className="Menu">
+            <List component="nav">
+              <ListItem button>
+                <ListItemIcon>
+                  <InfoIcon />
+                </ListItemIcon>
+                <ListItemText primary="About Me" />
+              </ListItem>
+                <ListItem button>
+                  <ListItemIcon>
+                  </ListItemIcon>
+                  <ListItemText primary="My Projects" />
+                </ListItem>
+            </List>
+            <Divider />
+            <List component="nav">
+              <ListItem button>
+                <ListItemText primary="Trash" />
+              </ListItem>
+            </List>
+      </div>
+
       </div>
     );
   }
