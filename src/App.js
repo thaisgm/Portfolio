b3fcd1468b636d04bf6588ea58a0ad4586807f2b
Thais Gonzalez <thaisgm@umich.edu>
2018-12-22 22:49:06 -0500

    Adding Material UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Text } from 'react';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <Text>About Me...</Text>
           <p>
             Thais Gonzalez
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
       </div>
+
     );
   }
 }
