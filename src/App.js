b903e83c7483ac2f31cea22b62b83eaea6939676
Thais Gonzalez <thaisgm@umich.edu>
2018-12-24 15:13:35 -0500

    bigger buttons

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ class App extends Component {
   render() {
     if (this.state.aboutMePage){
       return (
-        <div className = "MainPage" backgroundImage = './photos/stadium.jpeg'>
+        <div className = "MainPage" backgroundImage = {require('./photos/stadium.jpeg')}>
           <AppBar className = "menuBar" position="sticky" color= '#cce6ff'>
             <Toolbar variant="dense">
               <MenuItem className= "menuButton" onMouseDown = {() => this.back()}>
