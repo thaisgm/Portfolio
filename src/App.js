26aa71d012597180088f4c128a2ec9e24f596e23
Thais Gonzalez <thaisgm@umich.edu>
2018-12-27 23:38:01 -0500

    gifter side

--- src/App.js
+++ src/App.js
@@ -331,8 +331,8 @@ class GifterPage extends Component {
             <div className = "item2">
               <div className = "flexContainer2">
                 <div className = "flex3">
-                  <img className = "sfPic" src = {require('./photos/fuzzies.png')} alt = "not loading" width = '325px'/>
-                  <img className = "sfPic" src = {require('./photos/twillio.PNG')} alt = "not loading" width = '325px'/>
+                  <img className = "sfPic" src = {require('./photos/fuzzies.png')} alt = "not loading" width = '165px'/>
+                  <img className = "sfPic" src = {require('./photos/twillio.PNG')} alt = "not loading" width = '165px'/>
                 </div>
               <span className = "caption">Our colorful and short quiz for parents.</span>
               </div>
